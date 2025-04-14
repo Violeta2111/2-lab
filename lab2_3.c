d92b46f228ad0bc2026649bde2c1584af137da6a
Violeta <vakarvioletka@gmail.com>
2025-04-14 14:08:56 +0300

    савм

--- lab2_3.c
+++ lab2_3.c
@@ -11,21 +11,11 @@ int main() {
     printf("Введіть значення цілого числа z: ");
     scanf("%d", &z);
 
-    // Перевірка на нуль у знаменнику
-    if (z - 2 * y == 0) {
-        printf("Помилка: знаменник (z - 2y) дорівнює нулю.\n");
-        return 1;
-    }
 
     // Обчислення виразу під коренем
     double fraction = 3.0 * y / (z - 2.0 * y);
     double expression = fraction - y * z;
 
-    // Перевірка, щоб уникнути sqrt з від'ємного
-    if (expression < 0) {
-        printf("Помилка: підкореневий вираз від’ємний: %.3f\n", expression);
-        return 1;
-    }
 
     // Обчислення кореня
     t = sqrt(expression);
